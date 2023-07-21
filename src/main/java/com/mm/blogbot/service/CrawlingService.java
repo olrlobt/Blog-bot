a8e0cee7310cb53cb10e57d3f6d930eaecc5d1f6
olrlobt <317tmdgjs@naver.com>
2023-07-21 20:02:27 +0900

    feat: 포스팅 제목, 작성시간 크롤링 추가

--- src/main/java/com/mm/blogbot/service/CrawlingService.java
+++ src/main/java/com/mm/blogbot/service/CrawlingService.java
@@ -1,26 +1,59 @@
 package com.mm.blogbot.service;
 
-import org.jsoup.Connection;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import java.io.IOException;
 
 @Service
 public class CrawlingService {
 
-    public void getPostTitle() throws IOException {
-        String URL = "https://olrlobt.tistory.com/rss";
+    private final String URL = "https://olrlobt.tistory.com/rss";
+    @Value("${blog.title}")
+    private String titleClassName;
+    @Value("${blog.date}")
+    private String dateClassName;
 
-        Connection conn = Jsoup.connect(URL);
-        Document document = conn.get();
 
-        Elements items = document.select("item title");
-        for (Element title : items) {
-            System.out.println(title.text());
+    public void getPost() throws IOException {
+        Document document = Jsoup.connect(URL).get();
+        getPostTitle(document);
+        getPostDate(document);
+    }
+
+
+    private String getPostTitle(Document document) throws IOException {
+        Elements titles = document.select(titleClassName);
+        return titles.get(0).text();
+    }
+
+
+    private String getPostDate(Document document) {
+        Elements dates = document.select(dateClassName);
+        try {
+            String inputDateStr = dates.get(0).text();
+            SimpleDateFormat inputFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z", Locale.ENGLISH);
+            Date date = inputFormat.parse(inputDateStr);
+
+            SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+            // UTC 시간대로 설정
+            outputFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+            return outputFormat.format(date);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
+
+
+
+
+
 }
