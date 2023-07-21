e7904ac3ad740ded3354a64d336c4f0b7e9fff50
olrlobt <317tmdgjs@naver.com>
2023-07-21 14:45:10 +0900

    feat: 프로젝트생성

---
+++ src/main/java/com/mm/blogbot/BlogBotApplication.java
@@ -0,0 +1,13 @@
+package com.mm.blogbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogBotApplication.class, args);
+	}
+
+}
